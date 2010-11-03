621c27ab9ef51e4fe5920cbaa636e397e4f52412
Brian Muller <brian.muller@livingsocial.com>
2010-11-03 07:34:49 -0400

    updated URL's in all files to github

--- mod_auth_openid.cpp
+++ mod_auth_openid.cpp
@@ -469,39 +469,28 @@ static int mod_authopenid_check_user_access(request_rec *r) {
   
   reqs = (require_line *)reqs_arr->elts;
   for (x = 0; x < reqs_arr->nelts; x++) {
-
     if (!(reqs[x].method_mask & (AP_METHOD_BIT << m))) 
       continue;
 
     t = reqs[x].requirement;
     w = ap_getword_white(r->pool, &t);
-    if (!strcasecmp(w, "valid-user")) {
+    if (!strcasecmp(w, "valid-user"))
       return OK;
-    }
+
     if (!strcasecmp(w, "user")) {
-      /* And note that there are applicable requirements                                                                                                                                                                               
-       * which we consider ourselves the owner of.                                                                                                                                                                                     
-       */
       required_user = 1;
       while (t[0]) {
 	w = ap_getword_conf(r->pool, &t);
-	if (!strcmp(user, w)) {
+	if (!strcmp(user, w))
 	  return OK;
-	}
       }
     }
   }
 
-  if (!required_user) {
-    /* no applicable requirements */
+  if (!required_user)
     return DECLINED;
-  }
 
-  ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
-                  "access to %s failed, reason: user '%s' does not meet "
-		"'require'ments for user/valid-user to be allowed access",
-		r->uri, user);
-
+  APERR(r, "Access to %s failed: user '%s' invalid", r->uri, user);
   ap_note_auth_failure(r);
   return HTTP_UNAUTHORIZED;
 }
